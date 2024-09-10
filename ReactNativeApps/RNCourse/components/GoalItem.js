ffe63c61fc2540dfbfeb57688f63260bf5b4943c
SwamyPeddinti <swamypeddinti@outlook.com>
2024-09-10 12:25:32 +0530

    ReactNativeApps RNCourse Inprogress

---
+++ ReactNativeApps/RNCourse/components/GoalItem.js
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+export default function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItemr}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    color: "white",
+    padding: 8,
+  },
+});
